36de26f87a91c527a6d66b4290882acfa1093c23
Salvador Fandiño García <salva@cpan.org>
2010-09-22 09:20:33 +0000

    Import of SALVA/Net-SFTP-Foreign-1.61 from CPAN.
    gitpan-cpan-distribution: Net-SFTP-Foreign
    gitpan-cpan-version:      1.61
    gitpan-cpan-path:         SALVA/Net-SFTP-Foreign-1.61.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     released

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -1,6 +1,6 @@
 package Net::SFTP::Foreign;
 
-our $VERSION = '1.60';
+our $VERSION = '1.61';
 
 use strict;
 use warnings;
@@ -638,9 +638,6 @@ sub _write {
     my @written;
     my $written = 0;
     my $end;
-
-    my $selin = '';
-    vec($selin, fileno($sftp->{ssh_in}), 1) = 1;
 
     while (!$end or @msgid) {
 	while (!$end and @msgid < $qsize) {
@@ -1517,13 +1514,9 @@ sub get {
     my @msgid;
     my @askoff;
     my $loff = $askoff;
-    my $rfno = fileno($sftp->{ssh_in});
     my $adjustment = 0;
-    my $selin = '';
     my $n = 0;
     local $\;
-
-    vec ($selin, $rfno, 1) = 1;
 
     while (1) {
 	# request a new block if queue is not full
@@ -1910,7 +1903,6 @@ sub put {
     # if lsize is undef, we initialize it to $writeoff:
     $lsize += $writeoff if ($append or not defined $lsize);
 
-    my $rfno = fileno($sftp->{ssh_in});
     # when a converter is used, the EOF can become delayed by the
     # buffering introduced, we use $eof_t to account for that.
     my ($eof, $eof_t);
