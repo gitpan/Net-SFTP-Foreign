dcc67ec172477a7f1a30f3812445fd671d5ced40
Salvador Fandiño García <salva@cpan.org>
2012-03-14 10:04:18 +0000

    Import of SALVA/Net-SFTP-Foreign-1.71 from CPAN.
    gitpan-cpan-distribution: Net-SFTP-Foreign
    gitpan-cpan-version:      1.71
    gitpan-cpan-path:         SALVA/Net-SFTP-Foreign-1.71.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     released

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -1,6 +1,6 @@
 package Net::SFTP::Foreign;
 
-our $VERSION = '1.70_10';
+our $VERSION = '1.71';
 
 use strict;
 use warnings;
@@ -334,6 +334,15 @@ sub _init {
                 my $key = $msg->get_str;
                 my $value = $msg->get_str;
                 $sftp->{server_extensions}{$key} = $value;
+
+                if ($key eq 'vendor-id') {
+                    my $vid = Net::SFTP::Foreign::Buffer->make("$value");
+                    $sftp->{_ext__vendor_id} = [ Encode::decode(utf8 => $vid->get_str),
+                                                 Encode::decode(utf8 => $vid->get_str),
+                                                 Encode::decode(utf8 => $vid->get_str),
+                                                 $vid->get_int64 ];
+                }
+
             }
 
 	    return $version;
