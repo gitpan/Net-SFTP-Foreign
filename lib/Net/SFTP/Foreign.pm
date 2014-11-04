329e01332c88118c747dd9bbf37bc0478c551703
Salvador Fandiño García <salva@cpan.org>
2014-11-04 14:36:38 +0000

    Import of SALVA/Net-SFTP-Foreign-1.78_02 from CPAN.
    gitpan-cpan-distribution: Net-SFTP-Foreign
    gitpan-cpan-version:      1.78_02
    gitpan-cpan-path:         SALVA/Net-SFTP-Foreign-1.78_02.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     developer

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -1,6 +1,6 @@
 package Net::SFTP::Foreign;
 
-our $VERSION = '1.78_01';
+our $VERSION = '1.78_02';
 
 use strict;
 use warnings;
@@ -214,6 +214,7 @@ sub new {
 
     $sftp->{_autodie} = delete $opts{autodie};
     $sftp->{_block_size} = delete $opts{block_size} || $defs{block_size} || 32*1024;
+    $sftp->{_min_block_size} = delete $opts{min_block_size} || $defs{min_block_size} || 512;
     $sftp->{_queue_size} = delete $opts{queue_size} || $defs{queue_size} || 32;
     $sftp->{_read_ahead} = $defs{read_ahead} || $sftp->{_block_size} * 4;
     $sftp->{_write_delay} = $defs{write_delay} || $sftp->{_block_size} * 8;
@@ -502,25 +503,32 @@ sub _check_status_ok {
 }
 
 sub setcwd {
-    @_ <= 2 or croak 'Usage: $sftp->setcwd($path)';
     ${^TAINT} and &_catch_tainted_args;
 
-    my ($sftp, $cwd) = @_;
+    my ($sftp, $cwd, %opts) = @_;
     $sftp->_clear_error_and_status;
 
+    my $check = delete $opts{check};
+    $check = 1 unless defined $check;
+
+    %opts and _croak_bad_options(keys %opts);
+
     if (defined $cwd) {
-        $cwd = $sftp->realpath($cwd);
-        return undef unless defined $cwd;
-	my $a = $sftp->stat($cwd)
-	    or return undef;
-	if (_is_dir($a->perm)) {
-	    return $sftp->{cwd} = $cwd;
-	}
-	else {
-	    $sftp->_set_error(SFTP_ERR_REMOTE_BAD_OBJECT,
-			      "Remote object '$cwd' is not a directory");
-	    return undef;
-	}
+        if ($check) {
+            $cwd = $sftp->realpath($cwd);
+            return undef unless defined $cwd;
+            my $a = $sftp->stat($cwd)
+                or return undef;
+            unless (_is_dir($a->perm)) {
+                $sftp->_set_error(SFTP_ERR_REMOTE_BAD_OBJECT,
+                                  "Remote object '$cwd' is not a directory");
+                return undef;
+            }
+        }
+        else {
+            $cwd = $sftp->_rel2abs($cwd);
+        }
+        return $sftp->{cwd} = $cwd;
     }
     else {
         delete $sftp->{cwd};
@@ -1728,10 +1736,11 @@ sub get {
     my @askoff;
     my $loff = $askoff;
     my $adjustment = 0;
-    my $n = 0;
     local $\;
 
     my $slow_start = ($size == -1 ? $queue_size - 1 : 0);
+
+    my $safe_block_size = $sftp->{_min_block_size} >= $block_size;
 
     do {
         # Disable autodie here in order to do not leave unhandled
@@ -1743,15 +1752,14 @@ sub get {
 
         while (1) {
             # request a new block if queue is not full
-            while (!@msgid or ( ($size == -1 or $size > $askoff)   and
+            while (!@msgid or ( ($size == -1 or $size + $block_size > $askoff)   and
                                 @msgid < $queue_size - $slow_start and
-                                $n != 1 ) ) {
+                                $safe_block_size ) ) {
                 my $id = $sftp->_queue_new_msg(SSH2_FXP_READ, str=> $rfid,
                                                int64 => $askoff, int32 => $block_size);
                 push @msgid, $id;
                 push @askoff, $askoff;
                 $askoff += $block_size;
-                $n++;
             }
 
             $slow_start-- if $slow_start;
@@ -1764,10 +1772,7 @@ sub get {
                                                 "Couldn't read from remote file");
 
             unless ($msg) {
-                if ($sftp->{_status} == SSH2_FX_EOF) {
-                    $sftp->_set_error;
-                    $roff != $loff and next;
-                }
+                $sftp->_set_error if $sftp->{_status} == SSH2_FX_EOF;
                 last;
             }
 
@@ -1781,9 +1786,16 @@ sub get {
             }
 
             $loff += $len;
-            if ($len < $block_size) {
-                $block_size = $len < 2048 ? 2048 : $len;
-                $askoff = $loff;
+            unless ($safe_block_size) {
+                if ($len > $sftp->{_min_block_size}) {
+                    $sftp->{min_block_size} = $len;
+                    if ($len < $block_size) {
+                        # auto-adjust block size
+                        $block_size = $len;
+                        $askoff = $loff;
+                    }
+                }
+                $safe_block_size = 1;
             }
 
             my $adjustment_before = $adjustment;
@@ -3911,12 +3923,31 @@ Returns the remote current working directory.
 When a relative remote path is passed to any of the methods on this
 package, this directory is used to compose the absolute path.
 
-=item $sftp-E<gt>setcwd($dir)
+=item $sftp-E<gt>setcwd($dir, %opts)
 
 Changes the remote current working directory. The remote directory
 should exist, otherwise the call fails.
 
 Returns the new remote current working directory or undef on failure.
+
+Passing C<undef> as the C<$dir> argument resets the cwd to the server
+default which is usually the user home but not always.
+
+The method accepts the following options:
+
+=over 4
+
+=item check => 0
+
+By default the given target directory is checked against the remote
+server to ensure that it actually exists and that it is a
+directory. Some servers may fail to honor those requests even for
+valid directories (i.e. when the directory has the hidden flag set).
+
+This option allows to disable those checks and just sets the cwd to
+the given value blindly.
+
+=back
 
 =item $sftp-E<gt>get($remote, $local, %options)
 
